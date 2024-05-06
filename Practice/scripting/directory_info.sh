dbcb23b3c164aeabc3aa50b3b5797fbc9b273487
evontae <evontae@mail.com>
2024-05-05 19:01:08 -0500

    Completed project to write a script to check a directory and save the files in a directory to a txt file, similar to the exercise but with conditional logic

---
+++ Practice/scripting/directory_info.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script Creation (directory_info.sh):
+# Write a script that:
+# Checks if a specified directory exists.
+# Lists its contents including hidden files.
+# Outputs the details to a file with a time-stamped filename.
+# Changes permissions to ensure the script is executable.
+
+# initialize variable for directory
+directory="/Users/evo/path/DevOps-SRE-Journey/Practice/scripting"
+
+# Use if statement to check to see if the directory exsists, if not then create it
+if [ ! -d "$directory" ]; then
+    mkdir "$directory"
+fi 
+
+# List all the elements in the variabel directory then redirect to current_directory.txt file
+ls -at > "$directory/current_saved_directory.txt"
